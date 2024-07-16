8b247ca309548bc35dbf13810b734034ed9c2412
Matheus Gomes <math.s.gomes@gmail.com>
2024-07-15 23:36:50 -0300

    add composer to up database

--- TravelListApp.Persistence/Contexts/TravelListDbContext.cs
+++ TravelListApp.Persistence/Contexts/TravelListDbContext.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TravelListApp.Domain.Entities;
+
 namespace TravelListApp.Persistence.Contexts;
 
-public class TravelListDbContext
+public class TravelListDbContext : DbContext
 {
+    public DbSet<Travel> Travels { get; set; }
     
+    public TravelListDbContext(DbContextOptions<TravelListDbContext> options) : base(options) { }
 }
