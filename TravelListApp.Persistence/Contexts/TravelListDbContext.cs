c9b5f21158b1704b3026a55b3d60d7964b378e21
Matheus Gomes <math.s.gomes@gmail.com>
2024-07-16 23:24:46 -0300

    chore: settings postgres database

--- TravelListApp.Persistence/Contexts/TravelListDbContext.cs
+++ TravelListApp.Persistence/Contexts/TravelListDbContext.cs
@@ -8,4 +8,10 @@ public class TravelListDbContext : DbContext
     public DbSet<Travel> Travels { get; set; }
     
     public TravelListDbContext(DbContextOptions<TravelListDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Travel>().ToTable("travels");
+    }
 }
