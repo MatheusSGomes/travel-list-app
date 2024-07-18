8fd855cff81a9c9c1a97774d1c650494df9c1552
Matheus Gomes <math.s.gomes@gmail.com>
2024-07-17 23:43:45 -0300

    entity travel columns and table names

--- TravelListApp.Domain/Entities/Travel.cs
+++ TravelListApp.Domain/Entities/Travel.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TravelListApp.Domain.Entities;
 
+[Table("travels", Schema = "public")]
 public class Travel
 {
+    [Column("id")]
     public Guid Id { get; private set; }
+
+    [Column("destination")]
     public string Destination { get; private set; }
+    
+    [Column("start_date")]
     public DateTime StartDate { get; private set; }
+    
+    [Column("end_date")]
     public DateTime EndDate { get; private set; }
+    
+    [Column("cost")]
     public decimal Cost { get; private set; }
 
     public Travel(string destination, DateTime startDate, DateTime endDate, decimal cost)
@@ -16,7 +28,7 @@ public class Travel
         EndDate = endDate;
         Cost = cost;
     }
-
+    
     public void UpdateDetails(string destination, DateTime startDate, DateTime endDate, decimal cost)
     {
         Destination = destination;
